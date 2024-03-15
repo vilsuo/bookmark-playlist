858a1ee9c01f3beb796b8d8d600a199fd55db487
vilsuo <vilsuo1@gmail.com>
2024-03-15 21:57:31 +0200

    added types to link parser test elements

--- server/test/util.ts/linkParser.spec.ts
+++ server/test/util.ts/linkParser.spec.ts
@@ -2,10 +2,92 @@
 
 type Attributes = Record<string, string>;
 
+type HeaderType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+type ElementType = 'a' | 'p' | HeaderType | 'dl' | 'dt' | 'body';
+
 type HtmlElement = {
-  tag: string;
+  tag: ElementType;
   attributes?: Attributes;
   content: string | HtmlElement | Array<HtmlElement>;
+};
+
+/**
+ * <a href="somelink" { ...attributes }>{content}</a>
+ */
+type LinkElement = {
+  tag: 'a';
+  attributes: Attributes & { href: string; };
+  content: string;
+};
+
+/**
+ * <p>{content}</p>
+ */
+type ParagraphElement = {
+  tag: 'p';
+  content: string;
+};
+
+/**
+ * <h? { ...attributes }>{content}</h?>
+ */
+type HeaderElement = {
+  tag: HeaderType;
+  attributes?: Attributes;
+  content: string;
+};
+
+/**
+ * <dt>
+ *    <a />
+ * </dt>
+ */
+type DtSingleElement = {
+  tag: 'dt';
+  content: LinkElement; // HtmlElement
+};
+
+/**
+ * <dt>
+ *    <h? />
+ *    <dl />
+ *    <p />
+ * <dt>
+ */
+type DtFolderElement = {
+  tag: 'dt';
+  // one of each, in this order
+  content: [HeaderElement, DlElement, ParagraphElement]; // Array<HtmlElement>;
+};
+
+/**
+ * <dl>
+ *    <p />
+ *    <dt />
+ *    <dt />
+ *      .
+ *      .
+ *      .
+ *    <dt />
+ * </dl>
+ */
+type DlElement = {
+  tag: 'dl';
+  // exactly a single paragraph in the start
+  // rest should be Dt:s
+  content: [ParagraphElement, ...Array<DtSingleElement | DtFolderElement>]; // Array<HtmlElement>;
+};
+
+/**
+ * <body>
+ *    <h? />
+ *    <dl />
+ *    <p />
+ * </body>
+ */
+type BodyElement = {
+  tag: 'body';
+  content: [HeaderElement, DlElement, ParagraphElement];
 };
 
 const isElement = (value: unknown): value is HtmlElement => {
@@ -39,26 +121,102 @@ const parseElement = ({ tag, attributes, content }: HtmlElement) => {
   `;
 };
 
-const linkElements: Array<HtmlElement> = [
-  {
-    tag: 'a',
-    attributes: { href: 'http://localhost:3000/link1' },
-    content: 'Link1'
-  },
-  {
-    tag: 'a',
-    attributes: { href: 'http://localhost:3000/link2' },
-    content: 'Link2'
-  },
+// SINGLE 
+
+const createLinkElement = (href: string, text: string): LinkElement => ({
+  tag: 'a',
+  attributes: { href },
+  content: text
+});
+
+const createHeaderElement = (level: number, text: string): HeaderElement => {
+  const headers = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] as const;
+
+  return {
+    tag: headers[level - 1],
+    content: text,
+  };
+};
+
+const createParagraphElement = (text: string = ''): ParagraphElement => ({
+  tag: 'p',
+  content: text,
+});
+
+const createDtSingle = (linkElement: LinkElement): DtSingleElement => ({
+  tag: 'dt',
+  content: linkElement,
+});
+
+const createDtFolder = (level: number, headerText: string, dlElement: DlElement): DtFolderElement => ({
+  tag: 'dt',
+  content: [
+    createHeaderElement(level, headerText),
+    dlElement,
+    createParagraphElement(),
+  ],
+});
+
+const createDlElement = (dtElements: Array<DtFolderElement | DtSingleElement>): DlElement => ({
+  tag: 'dl',
+  content: [
+    createParagraphElement(),
+    ...dtElements,
+  ],
+});
+
+const createBodyElement = (level: number, headerText: string, dlElement: DlElement): BodyElement => ({
+  tag: 'body',
+  content: [
+    createHeaderElement(level, headerText),
+    dlElement,
+    createParagraphElement(),
+  ]
+});
+
+const linktTemplate = 'http://localhost:3000/some/addr/';
+
+// create 10 single dts with links inside
+const singles = [...Array(10).keys()].map(
+  int => createDtSingle(
+    createLinkElement(linktTemplate + int, 'Link' + int)
+  )
+);
+
+// create 3 dls for the folders
+const dls = [
+  createDlElement([singles[0]]),
+  createDlElement([singles[2], singles[3]]),
+  createDlElement([singles[5], singles[6], singles[7]]),
 ];
 
-const parentElement: HtmlElement = {
-  tag: 'dl',
-  content: linkElements,
-};
+// create 3 folder dts
+const folders = [
+  createDtFolder(3, 'folder 1', dls[0]),
+  createDtFolder(3, 'folder 2', dls[1]),
+  createDtFolder(3, 'folder 3', dls[2]),
+];
+
+const rootDl = createDlElement([
+  folders[0],
+  singles[1],
+  folders[1],
+  singles[4],
+  folders[2],
+  singles[8],
+  singles[9],
+]);
+
+const body = createBodyElement(1, 'Bookmarks', rootDl);
+
+// console.log('singles', singles);
+// console.log('dls', dls);
+// console.log('folders', folders);
+// console.log('rootDl', rootDl);
+console.log('body', body);
 
 describe('getHeaderRange', () => {
   it('initial test', () => {
-    console.log('test', parseElement(parentElement));
+    console.log('test', parseElement(body));
   });
 });
