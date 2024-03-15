6076d3a670fa4e2e96c4391725915a2913e2c05f
vilsuo <vilsuo1@gmail.com>
2024-03-16 01:47:46 +0200

    added initial parsing for link titles and hyperrefs

--- server/src/router/bookmark.ts
+++ server/src/router/bookmark.ts
@@ -5,6 +5,44 @@ import { singleUpload } from '../util/fileUpload';
 
 const router = express();
 
+type Link = {
+  title: string;
+  href: undefined | string;
+};
+
+const createAlbumsFromLinks = (links: Array<Link>) => {
+  const VIDEO_ID_LENGTH = 11;
+  const VIDEO_PREFIX = 'https://www.youtube.com/watch?v=';
+
+  //const pattern = new RegExp(`?v=(.${VIDEO_ID_LENGTH})`);
+  return links.map((link) => {
+    const { title, href } = link;
+    if (!href || !href.startsWith(VIDEO_PREFIX) || href.length < VIDEO_PREFIX.length + VIDEO_ID_LENGTH) {
+      throw new Error('Link has an incorrect href');
+    }
+
+    const videoId = href.substring(VIDEO_PREFIX.length, VIDEO_PREFIX.length + VIDEO_ID_LENGTH);
+
+    // artist - title (published)
+    const parts = title.split(' - ');
+    if (parts.length !== 2) {
+      throw new Error('Title does not have artist/title separator');
+    }
+
+    const otherParts = parts[1].split('(');
+    if (otherParts[1].length < 4 || isNaN(Number(otherParts[1].substring(0, 4)))) {
+      throw new Error('Invalid year');
+    }
+
+    return {
+      videoId,
+      artist: parts[0].trim(),
+      title: otherParts[0].trim(),
+      published: Number(otherParts[1].substring(0, 4)),
+    };
+  });
+};
+
 // TODO
 // - specify header in formData fields
 router.post('/', async (req: Request, res: Response, next: NextFunction) => {
@@ -24,8 +62,10 @@ router.post('/', async (req: Request, res: Response, next: NextFunction) => {
       // convert file to string
       const fileString = file.buffer.toString();
 
+      // convert file string to links
       const links = linkParser.getHeaderNextDlSiblingLinks(fileString, 'music');
-      return res.status(201).send(links);
+
+      return res.status(201).send(createAlbumsFromLinks(links));
     } catch (error) {
       console.log('Error', error);
       next(error);
