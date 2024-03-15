0b7113ae9b52a3296235fddcd0474094c532da29
vilsuo <vilsuo1@gmail.com>
2024-03-15 16:33:14 +0200

    created test helpers for html string creation

--- server/test/util.ts/linkParser.spec.ts
+++ server/test/util.ts/linkParser.spec.ts
@@ -1,6 +1,64 @@
+//import { getHeaderRange } from '../../src/util/linkParser';
 
-describe('describing', () => {
+type Attributes = Record<string, string>;
+
+type HtmlElement = {
+  tag: string;
+  attributes?: Attributes;
+  content: string | HtmlElement | Array<HtmlElement>;
+};
+
+const isElement = (value: unknown): value is HtmlElement => {
+  return (value !== null && typeof value === 'object' && 'tag' in value);
+};
+
+const isElementArray = (value: unknown): value is Array<HtmlElement> => {
+  return (Array.isArray(value) && value.filter(c => isElement(c)).length === value.length);
+};
+
+const parseAttributes = (attributes: Attributes) => {
+  return Object.keys(attributes)
+    .map((attr) => `${attr}="${attributes[attr]}"`)
+    .join(' ');
+};
+
+const parseElement = ({ tag, attributes, content }: HtmlElement) => {
+  const attributeString = attributes
+    ? ' ' + parseAttributes(attributes)
+    : '';
+
+  let children = '';
+  if (typeof content === 'string')  children = content;
+  else if (isElement(content))      children = parseElement(content);
+  else if (isElementArray(content)) children = content.map(c => parseElement(c)).join('');
+
+  return `
+    <${tag}${attributeString}>
+      ${children}
+    </${tag}>
+  `;
+};
+
+const linkElements: Array<HtmlElement> = [
+  {
+    tag: 'a',
+    attributes: { href: 'http://localhost:3000/link1' },
+    content: 'Link1'
+  },
+  {
+    tag: 'a',
+    attributes: { href: 'http://localhost:3000/link2' },
+    content: 'Link2'
+  },
+];
+
+const parentElement: HtmlElement = {
+  tag: 'dl',
+  content: linkElements,
+};
+
+describe('getHeaderRange', () => {
   it('initial test', () => {
-    expect(2 + 2).toBe(4);
+    console.log('test', parseElement(parentElement));
   });
 });
