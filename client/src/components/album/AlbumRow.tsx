c79c8092bd58f2b9495ff9f0c9409ac72d7e27ce
vilsuo <vilsuo1@gmail.com>
2024-03-17 17:10:47 +0200

    changed context menu component to more general dropdown component

--- client/src/components/album/AlbumRow.tsx
+++ client/src/components/album/AlbumRow.tsx
@@ -2,39 +2,55 @@ import { useState } from 'react';
 import { Album, Link } from '../../types';
 import { useComponentVisible } from '../../util/hooks';
 
-interface MenuContextProps {
-  text: string;
-  links: Array<Link>;
+interface ActionLinkProps {
+  link: Link;
+  onClick: () => void;
 }
 
-const MenuContext = ({ text, links }: MenuContextProps) => {
+const ActionLink = ({ link, onClick }: ActionLinkProps) => (
+  <a
+    onClick={onClick}
+    target='_blank' // to open in a new tab
+    href={link.href}
+  >
+    {link.text}
+  </a>
+);
+
+interface DropdownProps {
+  children: string | JSX.Element;
+  items: Link[];
+}
+
+const Dropdown = ({ children, items }: DropdownProps) => {
+  // outside clicks
   const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible(false);
 
+  // remember click location
   const [points, setPoints] = useState({ x: 0, y: 0 });
 
   return (
     <div ref={ref}>
       <div
         onClick={(e) => {
-          setIsComponentVisible(!isComponentVisible);
+          setIsComponentVisible(!isComponentVisible); // toggle context
           setPoints({ x: e.pageX, y: e.pageY });
         }}
       >
-        {text}
+        {children}
       </div>
 
       {isComponentVisible && (
-        <div className='menu-context-context'>
-          <ul style={{ top: points.y, left: points.x }}>
-            {links.map((link, idx) => (
+        <div className='dropdown-context'
+          style={{ top: points.y + 5, left: points.x - 5 }}
+        >
+          <ul>
+            {items.map((item, idx) => (
               <li key={idx}>
-                <a
+                <ActionLink
                   onClick={() => setIsComponentVisible(false)}
-                  href={link.href}
-                  target='_blank'
-                >
-                  {link.text}
-                </a>
+                  link={item}
+                />
               </li>
             ))}
           </ul>
@@ -56,17 +72,17 @@ const AlbumRow = ({ album, className, select }: AlbumRowProps) => {
   const titleSearchString = album.title.replace(' ', '+');
 
   const getArtistSearchLink = () => {
-    return {
-      text: album.artist,
-      href: `https://www.metal-archives.com/search?searchString=${artistSearchString}&type=band_name`
-    }
+    const text = album.artist;
+    const href = `https://www.metal-archives.com/search?searchString=${artistSearchString}&type=band_name`;
+    
+    return { text, href };
   };
 
   const getAlbumSearchLink = () => {
-    return {
-      text: album.title,
-      href: `https://www.metal-archives.com/search/advanced/searching/albums?bandName=${artistSearchString}&releaseTitle=${titleSearchString}`,
-    }
+    const text = album.title;
+    const href = `https://www.metal-archives.com/search/advanced/searching/albums?bandName=${artistSearchString}&releaseTitle=${titleSearchString}`;
+
+    return { text, href };
   };
 
   return (
@@ -74,17 +90,15 @@ const AlbumRow = ({ album, className, select }: AlbumRowProps) => {
       <td onClick={() => select(album)}>Play</td>
 
       <td>
-        <MenuContext
-          text={album.artist}
-          links={[getArtistSearchLink()]}
-        />
+        <Dropdown items={[getArtistSearchLink()]}>
+          {album.artist}
+        </Dropdown>
       </td>
 
       <td>
-        <MenuContext
-          text={album.title}
-          links={[getAlbumSearchLink()]}
-        />
+        <Dropdown items={[getAlbumSearchLink()]}>
+          {album.title}
+        </Dropdown>
       </td>
       <td>{album.published}</td>
     </tr>
