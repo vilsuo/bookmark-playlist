01478663472b6309d4f7fb8928957178e5208e63
vilsuo <vilsuo1@gmail.com>
2024-03-17 22:41:30 +0200

    refractored dropdown

--- client/src/components/album/AlbumRow.tsx
+++ client/src/components/album/AlbumRow.tsx
@@ -1,66 +1,7 @@
-import { useState } from 'react';
 import { Album, Link } from '../../types';
-import { useComponentVisible } from '../../util/hooks';
 
 import ma from '../../../assets/ma.ico';
-
-interface DropdownItemProps {
-  link: Link;
-  close: () => void;
-}
-
-const DropdownItem = ({ link, close }: DropdownItemProps) => {
-  const { text, href, imageSrc, className } = link;
-  return (
-    <li className={className} onClick={close}>
-      <a href={href} target='_blank'>
-        { imageSrc && <img src={imageSrc} /> }
-        <span>{text}</span>
-      </a>
-    </li>
-  )
-};
-
-interface DropdownProps {
-  children: string | JSX.Element;
-  links: Link[];
-}
-
-const Dropdown = ({ children, links }: DropdownProps) => {
-  // outside clicks
-  const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible(false);
-
-  // remember click location
-  const [points, setPoints] = useState({ x: 0, y: 0 });
-
-  return (
-    <div ref={ref}>
-      <div className='dropdown-trigger'
-        onClick={(e) => {
-          setIsComponentVisible(!isComponentVisible); // toggle context
-          setPoints({ x: e.pageX, y: e.pageY });
-        }}
-      >
-        {children}
-      </div>
-
-      {isComponentVisible && (
-        <div className='dropdown-context'
-          style={{ top: points.y + 5, left: points.x - 5 }}
-        >
-          <ul>
-            {links.map((link, idx) => (
-              <DropdownItem key={idx}
-                link={link}
-                close={() => setIsComponentVisible(false)}
-              />
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
-  );
-};
+import Dropdown from '../Dropdown';
 
 interface AlbumRowProps {
   album: Album;
