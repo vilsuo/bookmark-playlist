a8ace5903a01ce46758330eb2f2702078d8728e2
vilsuo <vilsuo1@gmail.com>
2024-03-19 01:23:56 +0200

    added sorting to album table

--- client/src/components/album/AlbumTable.tsx
+++ client/src/components/album/AlbumTable.tsx
@@ -3,6 +3,16 @@ import { Album } from '../../types';
 import AlbumRow from './AlbumRow';
 import ExtraRow from './ExtraRow';
 
+enum Order {
+  DESC = -1, // largest to smallest
+  ASC = 1,  // smallest to largest
+}
+
+enum Column {
+  ARTIST,
+  ALBUM,
+}
+
 interface LinkListProps {
   albums: Album[];
   playingAlbum: Album | null;
@@ -12,6 +22,9 @@ interface LinkListProps {
 const AlbumTable = ({ albums, playingAlbum, setPlayingAlbum } : LinkListProps) => {
   const [viewingAlbum, setViewingAlbum] = useState<Album | null>(null);
 
+  const [sortColumn, setSortColumn] = useState<Column>(Column.ARTIST);
+  const [sortOrder, setSortOrder] = useState<Order>(Order.ASC);
+
   const isPlaying = (album: Album) => {
     return playingAlbum !== null && playingAlbum.videoId === album.videoId;
   };
@@ -20,20 +33,53 @@ const AlbumTable = ({ albums, playingAlbum, setPlayingAlbum } : LinkListProps) =
     return viewingAlbum !== null && viewingAlbum.videoId === album.videoId;
   };
 
+  const handleSort = (colum: Column) => {
+    if (colum === sortColumn) {
+      setSortOrder(sortOrder === Order.ASC ? Order.DESC : Order.ASC);
+    }
+
+    setSortColumn(colum);
+  };
+
+  const sortedAlbums = albums.toSorted((a: Album, b: Album) => {
+    const aArtist = a.artist.toLowerCase();
+    const bArtist = b.artist.toLowerCase();
+
+    const aTitle = a.title.toLowerCase();
+    const bTitle = b.title.toLowerCase();
+
+    if (sortColumn === Column.ARTIST) {
+      if (aArtist === bArtist) {
+        return b.published - a.published;
+      }
+
+      return sortOrder * ((aArtist < bArtist) ? -1 : 1);
+    } else {
+      if (aTitle === bTitle) {
+        return (aArtist < bArtist) ? -1 : 1;
+      }
+
+      return sortOrder * ((aTitle < bTitle) ? -1 : 1);
+    }
+  });
+
   return (
     <div>
       <h2>Albums</h2>
+
+      <p>Column { sortColumn === Column.ARTIST ? 'artist' : 'album' }</p>
+      <p>Order { sortOrder === Order.ASC ? 'asc' : 'desc' }</p>
       
       <table className='album-table'>
         <thead>
           <tr>
-            <th>Artist</th>
-            <th>Album</th>
+            <th onClick={() => handleSort(Column.ARTIST)}>Artist</th>
+            <th onClick={() => handleSort(Column.ALBUM)}>Album</th>
             <th>Year</th>
           </tr>
         </thead>
         <tbody>
-          {albums.map((album) =>
+          {sortedAlbums.map((album) =>
             !isViewed(album) ? (
               <AlbumRow key={album.videoId}
                 album={album}
