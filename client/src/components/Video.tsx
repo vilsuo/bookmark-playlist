efe3cc1e69f0ed777387db2d2e4bd2d9a5e2e107
vilsuo <vilsuo1@gmail.com>
2024-03-20 16:38:25 +0200

    changed react-youtube to iframe

--- client/src/components/Video.tsx
+++ client/src/components/Video.tsx
@@ -1,4 +1,3 @@
-import YouTube, { YouTubeProps } from 'react-youtube';
 import { Album } from '../types';
 
 interface VideoProps {
@@ -8,32 +7,10 @@ interface VideoProps {
 const Video = ({ album }: VideoProps) => {
   const { videoId } = album;
   
-  const options = {
-    width: '945',
-    height: '540',
-  
-    // https://developers.google.com/youtube/player_parameters
-    playerVars: {
-      // This parameter indicates whether the video player controls are displayed
-      // 1 – Player controls display in the player.
-      controls: 1,
-    },
-  };
-
-  const onPlayerReady: YouTubeProps['onReady'] = (event) => {
-    // access to player in all event handlers via event.target
-    event.target.pauseVideo();
-  };
-
-  console.log('videoId', videoId);
-
   return (
-    <YouTube
-      videoId={videoId}
-      opts={options}
-      onReady={onPlayerReady}
-      id='video'
-    />
+    <div className='video'>
+      <iframe src={`https://www.youtube.com/embed/${videoId}`} />
+    </div>
   );
 };
 
