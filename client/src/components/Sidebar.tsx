a7b3357e46b823021a0ab5000ceadf7c76b9040f
vilsuo <vilsuo1@gmail.com>
2024-03-20 12:50:30 +0200

    added scroll options to sidebar

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import BookmarkForm from './BookmarkForm';
 
 // albums
@@ -20,24 +20,32 @@ interface SidebarProps {
   close: () => void;
 }
 
-// TODO
-// - add buttons to scroll to top/bottom of album table
-// - fix album table extra row when only 1 album
 const Sidebar = ({ handleUpload, albums, playingAlbum, setPlayingAlbum, close }: SidebarProps) => {
   const [showUpload, setShowUpload] = useState(false);
   const [filterOptions, setFilterOptions] = useState<FilterOptions>(DEFAULT_FILTER_OPTIONS);
+
+  const startRef = useRef<null | HTMLDivElement>(null);
+  const endRef = useRef<null | HTMLDivElement>(null);
+
+  const scrollTo = (ref: React.MutableRefObject<HTMLDivElement | null>) => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   const toggleUpload = () => setShowUpload(!showUpload);
 
   return (
     <div className='sidebar'>
       <div className='sidebar-toolbar'>
+        <button onClick={() => scrollTo(startRef)}>Up</button>
+        <button onClick={() => scrollTo(endRef)}>Down</button>
         <h2>Albums</h2>
         <button onClick={toggleUpload}>Toggle upload</button>
         <button onClick={close}>&#x2715;</button>
       </div>
 
       <div className='sidebar-container'>
+        <div id='start-ref' ref={startRef} />
+
         { showUpload && (
           <BookmarkForm upload={handleUpload} />
         )}
@@ -55,6 +63,7 @@ const Sidebar = ({ handleUpload, albums, playingAlbum, setPlayingAlbum, close }:
             filter={filterOptions.text.toLowerCase()}
           />
         )}
+        <div ref={endRef} />
       </div>
     </div>
   );
