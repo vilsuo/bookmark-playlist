6b692ffb0912b9e7bafc3003bbe942d39557aab6
vilsuo <vilsuo1@gmail.com>
2024-03-20 19:07:30 +0200

    fixed proken css property

--- client/src/components/AlbumsButton.tsx
+++ client/src/components/AlbumsButton.tsx
@@ -7,7 +7,7 @@ const AlbumsButton = ({ show }: AlbumsButtonProps) => {
   return (
     <button id='albums-button' onClick={show}>
       <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="20" cy="20" r="14" stroke="black" stroke-width="11" fill="none" />
+        <circle cx="20" cy="20" r="14" stroke="black" strokeWidth="11" fill="none" />
         <circle cx="20" cy="20" r="2" stroke="black" fill="black" />
       </svg> 
     </button>
