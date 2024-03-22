10cab7bb582b527027472af9fc8d02ed625af1e6
vilsuo <vilsuo1@gmail.com>
2024-03-22 14:50:07 +0200

    added settings store with redux toolkit. added video autoplay toggle to reducer

--- client/src/components/sidebars/settings/SettingsBar.tsx
+++ client/src/components/sidebars/settings/SettingsBar.tsx
@@ -1,14 +1,25 @@
+import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
+import { selectAutoplay, toggleAutoplay } from '../../../redux/reducers/settingsSlice';
 
 interface SettingsBarProps {
   close: () => void;
 }
 
 const SettingsBar = ({ close }: SettingsBarProps) => {
+  const dispatch = useAppDispatch()
+  const autoplay = useAppSelector(state => selectAutoplay(state));
+
   return (
     <div className='sidebar'>
       <div className='sidebar-toolbar'>
         <h2>Settings</h2>
         <button onClick={close}>&#x2715;</button>
+
+        <button
+          onClick={() => dispatch(toggleAutoplay())}
+        >
+          Toggle Autoplay { autoplay ? 'off' : 'on' }
+        </button>
       </div>
     </div>
   );
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Redux
+import { Provider } from 'react-redux';
+import { store } from './redux/store.ts';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
