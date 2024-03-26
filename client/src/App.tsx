a8921ef5cc1f2d8f9119bd53ddab892ca8417363
vilsuo <vilsuo1@gmail.com>
2024-03-26 23:11:56 +0200

    closing a video will play the next album in queue and remove it

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,22 @@
 import { useState } from 'react';
-
 import { Album, SidebarType,  } from './types';
 import * as bookmarksService from './util/bookmarksService';
 import VideoPlayer from './components/video/VideoPlayer';
 import SidebarOpener from './components/sidebars/opener/SidebarOpener';
 import Sidebar from './components/sidebars/Sidebar';
 import { useAppDispatch, useAppSelector } from './redux/hooks';
-import { play, selectPlaying } from './redux/reducers/albumsSlice';
+import { play, queuePop, selectPlaying, selectQueueFirst } from './redux/reducers/albumsSlice';
 import QueueTable from './components/queue/QueueTable';
 
 const Main = () => {
   const dispatch = useAppDispatch();
+  const playingAlbum = useAppSelector(selectPlaying);
+  const nextAlbumInQueue = useAppSelector(selectQueueFirst);
 
-  // playing
-  const playingAlbum = useAppSelector(selectPlaying)
-  const closeVideo = () => dispatch(play(null));
+  const closeVideo = () => {
+    dispatch(play(nextAlbumInQueue));
+    dispatch(queuePop());
+  };
 
   return (
     <div className="main">
