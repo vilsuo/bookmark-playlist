ba30685ae5feaf8bf2c8f7098ff5c4b1818aebc2
vilsuo <vilsuo1@gmail.com>
2024-03-26 16:58:55 +0200

    added queue to albumsslice

--- client/src/redux/reducers/albumsSlice.ts
+++ client/src/redux/reducers/albumsSlice.ts
@@ -1,37 +1,66 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSelector, createSlice } from '@reduxjs/toolkit';
 import { Album } from '../../types';
 import { RootState } from '../store';
 
 export interface AlbumsState {
   viewing: Album | null;
   playing: Album | null;
+  queue: Album[];
 };
 
 const initialState: AlbumsState = {
   viewing: null,
   playing: null,
+  queue: [],
 };
-
-type AlbumPayloadAction = PayloadAction<Album | null>;
 
 const albumsSlice = createSlice({
   name: 'albums',
   initialState,
   reducers: {
-    view: (state, action: AlbumPayloadAction) => {
+    view: (state, action: PayloadAction<Album | null>) => {
       const album = action.payload;
       state.viewing = album;
     },
-    play: (state, action: AlbumPayloadAction) => {
+    play: (state, action: PayloadAction<Album | null>) => {
       const album = action.payload;
       state.playing = album;
     },
+    pushQueue: (state, action: PayloadAction<Album>) => {
+      const albumToAdd = action.payload;
+
+      const found = state.queue.find(
+        (album) => album.videoId === albumToAdd.videoId
+      );
+      if (!found) {
+        state.queue.push(albumToAdd);
+      }
+    },
+    removeQueue: (state, action: PayloadAction<Album>) => {
+      const albumToRemove = action.payload;
+      state.queue = state.queue.filter(
+        (album) => album.videoId !== albumToRemove.videoId
+      );
+    }
   },
 });
 
-export const { view, play } = albumsSlice.actions;
+export const { view, play, pushQueue, removeQueue } = albumsSlice.actions;
 
 export const selectViewing = (state: RootState) => state.albums.viewing;
 export const selectPlaying = (state: RootState) => state.albums.playing;
+export const selectQueue = (state: RootState) => state.albums.queue;
+
+export const selectQueueFirst = (state: RootState) => {
+  if (state.albums.queue.length > 0) {
+    return state.albums.queue[0];
+  }
+  return null;
+};
+
+export const selectExistsInQueue = createSelector(
+  [selectQueue, (_state, album) => album],
+  (queue, album) => queue.find((q) => q.videoId === album.videoId) !== undefined,
+);
 
 export default albumsSlice.reducer;
