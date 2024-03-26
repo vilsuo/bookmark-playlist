c182f810c23b00efdfdd90a73c1e5c3dd56a7bdc
vilsuo <vilsuo1@gmail.com>
2024-03-26 16:59:34 +0200

    added queing option to albumsview component

--- client/src/components/sidebars/albums/AlbumsView.tsx
+++ client/src/components/sidebars/albums/AlbumsView.tsx
@@ -1,4 +1,6 @@
+import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
 import { Album } from '../../../types';
+import { pushQueue, selectExistsInQueue } from '../../../redux/reducers/albumsSlice';
 
 interface AlbumsViewProps {
   album: Album;
@@ -7,6 +9,13 @@ interface AlbumsViewProps {
 }
 
 const AlbumsView = ({ album, close, play }: AlbumsViewProps) => {
+  const dispatch = useAppDispatch();
+  const inQueue = useAppSelector((state) => selectExistsInQueue(state, album));
+
+  const addToQueue = () => {
+    dispatch(pushQueue(album));
+  };
+
   return (
     <div className="albums-view">
       <div className="header">
@@ -17,6 +26,7 @@ const AlbumsView = ({ album, close, play }: AlbumsViewProps) => {
       <div className="content">
         <div className="actions">
           <button onClick={play}>Play</button>
+          <button disabled={inQueue} onClick={addToQueue}>Queue</button>
         </div>
         <div className="details">
           <span className="artist">{album.artist}</span>
