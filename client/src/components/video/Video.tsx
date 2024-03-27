8f5dcfe8063d586055b0941d5911a60ef8b598c5
vilsuo <vilsuo1@gmail.com>
2024-03-27 13:31:59 +0200

    changed iframe to react-youtube

--- client/src/components/video/Video.tsx
+++ client/src/components/video/Video.tsx
@@ -1,51 +1,140 @@
 import { useAppSelector } from '../../redux/hooks';
 import { selectAutoplay } from '../../redux/reducers/settingsSlice';
-import { getYoutubeEmbedLink } from '../../util/links';
+import YouTube, { YouTubeProps } from 'react-youtube';
 
-interface VideoProps {
-  videoId: string;
-}
+enum PlayerState {
+  UNSTARTED = -1,
+  ENDED = 0,
+  PLAYING = 1,
+  PAYSED = 2,
+  BUFFERING = 3,
+  VIDEO_CUED = 5,
+};
 
 const ORIGIN = 'http://localhost:5173';
 
-const BASE_ATTRIBUTES = {
+/**
+ * https://developers.google.com/youtube/player_parameters
+ */
+const BASE_PLAYER_VARS = {
+  // autoplay
+  // cc_lang_pref
+  // cc_load_policy
+  // color
+  // controls
+  // disablekb
+  
   // enables the player to be controlled via IFrame Player API calls
   enablejsapi: 1,
 
+  // end
+  
   // prevent the fullscreen button from displaying in the player
   fs: 0,
 
+  // hl
+  // iv_load_policy
+  // list
+  // listType
+  // loop
+  // modestbranding
+  
   // This parameter provides an extra security measure for the IFrame API and
   // is only supported for IFrame embeds. If you are using the IFrame API, which
   // means you are setting the enablejsapi parameter value to 1, you should always
   // specify your domain as the origin parameter value.
   origin, ORIGIN,
+
+  // playlist
+  // playsinline
+  // rel
+  // start
+  // widget_referrer
 };
+
+interface VideoProps {
+  videoId: string;
+}
 
 const Video = ({ videoId }: VideoProps) => {
   const autoPlay = useAppSelector(selectAutoplay);
 
-  const attributes = {
-    ...BASE_ATTRIBUTES,
+  const opts: YouTubeProps['opts'] = {
+    // width
+    // height
+    // videoId
+    playerVars: {
+      ...BASE_PLAYER_VARS,
 
-    autoplay: +autoPlay,
+      autoplay: +autoPlay,
+      loop: 0,
+    },
+    // events,
+  };
 
-    loop: 0,
+  /**
+   * This event fires whenever a player has finished loading and is ready
+   * to begin receiving API calls.
+   * {@link https://developers.google.com/youtube/iframe_api_reference#onReady}
+   * 
+   * @param event 
+   */
+  const onReady: YouTubeProps['onReady'] = (event) => {
+    console.log('Ready');
+  };
 
-    /*
-    This parameter specifies a comma-separated list of video IDs to play.
-    If you specify a value, the first video that plays will be the VIDEO_ID
-    specified in the URL path, and the videos specified in the playlist
-    parameter will play thereafter. ???
+  /**
+   * This event fires if an error occurs in the player.
+   * {@link https://developers.google.com/youtube/iframe_api_reference#onError}
+   * 
+   * @param event 
+   */
+  const onError: YouTube['onError'] = (event) => {
+    console.log('Error');
+  };
 
-    playlist
-    */
+  /**
+   * This event fires when the layer's state changes to PlayerState.PLAYING.
+   * 
+   * @param event 
+   */
+  const onPlay: YouTubeProps['onPlay'] = (event) => {
+  };
+
+  /**
+   * This event fires when the layer's state changes to PlayerState.PAUSED.
+   * 
+   * @param event 
+   */
+  const onPause: YouTubeProps['onPause'] = (event) => {
+  };
+
+  /**
+   * This event fires when the layer's state changes to PlayerState.ENDED.
+   * 
+   * @param event 
+   */
+  const onEnd: YouTubeProps['onEnd'] = (event) => {
+    console.log('End');
+  };
+
+  /**
+   * This event fires whenever the player's state changes.
+   * {@link https://developers.google.com/youtube/iframe_api_reference#onStateChange}
+   * 
+   * @param event 
+   */
+  const onStateChange: YouTubeProps['onStateChange'] = (event) => {
   };
 
   return (
     <div className="video">
-      <iframe
-        src={getYoutubeEmbedLink(videoId, attributes)}
+      <YouTube
+        videoId={videoId}
+        opts={opts}
+        onReady={onReady}
+        onError={onError}
+        onEnd={onEnd}
       />
     </div>
   );
