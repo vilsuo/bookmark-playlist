6f81be343222341c1535cade3e1910a81d90bc12
vilsuo <vilsuo1@gmail.com>
2024-04-11 22:02:52 +0300

    created first editing popup

--- client/src/components/sidebars/albums/AlbumsView.tsx
+++ client/src/components/sidebars/albums/AlbumsView.tsx
@@ -1,6 +1,24 @@
+import { useState } from 'react';
 import { useAppDispatch } from '../../../redux/hooks';
 import { queueAdd } from '../../../redux/reducers/queueSlice';
 import { Album } from '../../../types';
+
+interface AlbumEditProps {
+  album: Album;
+  close: () => void;
+}
+
+const AlbumEdit = ({ album, close }: AlbumEditProps) => {
+  return (
+    <div className="editor">
+      <div className="header">
+        <h2>Edit</h2>
+        <button onClick={close}>&#x2715;</button>
+      </div>
+      <p>Editing {album.artist} - {album.title}</p>
+    </div>
+  );
+};
 
 interface AlbumsViewProps {
   album: Album;
@@ -9,28 +27,45 @@ interface AlbumsViewProps {
 }
 
 const AlbumsView = ({ album, close, play }: AlbumsViewProps) => {
+  const [editorOpen, setEditorOpen] = useState(false);
+
   const dispatch = useAppDispatch();
   const addToQueue = () => dispatch(queueAdd(album));
 
+  const openEdit = () => {
+    console.log('editing', album);
+    setEditorOpen(!editorOpen);
+  };
+
   return (
-    <div className="albums-view">
-      <div className="header">
-        <h3>Selected Album</h3>
-        <button onClick={close}>&#x2715;</button>
-      </div>
+    <>
+      { editorOpen && (
+        <AlbumEdit
+          album={album}
+          close={() => setEditorOpen(false)}
+        />
+      )}
 
-      <div className="content">
-        <div className="actions">
-          <button className="play-button" onClick={play}>Play</button>
-          <button onClick={addToQueue}>Q</button>
+      <div className="albums-view">
+        <div className="header">
+          <h3>Selected Album</h3>
+          <button onClick={close}>&#x2715;</button>
         </div>
 
-        <div className="details">
-          <span className="artist">{album.artist}</span>
-          <span className="title">{album.title}</span>
+        <div className="content">
+          <div className="actions">
+            <button className="play-button" onClick={play}>Play</button>
+            <button onClick={addToQueue}>Q</button>
+            <button onClick={openEdit}>Edit</button>
+          </div>
+
+          <div className="details">
+            <span className="artist">{album.artist}</span>
+            <span className="title">{album.title}</span>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
