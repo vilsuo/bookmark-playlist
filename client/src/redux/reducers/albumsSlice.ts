abf6af07548005b495a3fd3f1ca001b9e884e8bc
vilsuo <vilsuo1@gmail.com>
2024-06-15 17:46:48 +0300

    sorted categories without case

--- client/src/redux/reducers/albumsSlice.ts
+++ client/src/redux/reducers/albumsSlice.ts
@@ -152,7 +152,9 @@ export const selectPlaying = (state: RootState) => state.albums.playing;
 export const selectAlbums = (state: RootState) => state.albums.albums;
 
 export const selectCategories = createSelector(selectAlbums, (albums) => {
-  return Array.from(new Set(albums.map(album => album.category))).sort();
+  return Array.from(new Set(albums.map(album => album.category))).sort(
+    (a, b) => a.toLowerCase() > b.toLowerCase() ? 1 : -1
+  );
 });
 
 export default albumsSlice.reducer;
