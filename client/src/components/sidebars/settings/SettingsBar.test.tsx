cb576a7c03683a33a75c9ed3f049a68cf28ac13f
vilsuo <vilsuo1@gmail.com>
2024-07-10 14:27:15 +0300

    added state checks for settings bar tests

--- client/src/components/sidebars/settings/SettingsBar.test.tsx
+++ client/src/components/sidebars/settings/SettingsBar.test.tsx
@@ -5,7 +5,7 @@ import { fireEvent, screen } from '@testing-library/react';
 import { renderWithProviders } from '../../../../test/testUtils';
 
 import SettingsBar from "./SettingsBar";
-import { initialState as initialSettingsState } from '../../../redux/reducers/settingsSlice';
+import { initialState as initialSettingsState, selectAutoplay, selectPlayMode } from '../../../redux/reducers/settingsSlice';
 import { PlayMode } from '../../../types';
 import { changeOptionByLabel, findInputByLabelMatcher } from '../../../../test/uiHelpers';
 
@@ -15,13 +15,13 @@ describe("<SettingsBar />", () => {
   const autoPlayMatcher = /Autoplay/i;
   const playmodeMatcher = /Play mode/i;
 
-  test("Should render settings bar", () => {
+  test("should render settings bar", () => {
     renderWithProviders(<SettingsBar close={mockClose} />);
 
     screen.getByRole("heading", { name: "Settings" });
   });
 
-  test("Correct default options should be selected", async () => {
+  test("should select default options", async () => {
     renderWithProviders(<SettingsBar close={mockClose} />);
 
     expect((await findInputByLabelMatcher(autoPlayMatcher)).checked)
@@ -38,31 +38,38 @@ describe("<SettingsBar />", () => {
     );
   });
 
-  test("Can toggle settings", async () => {
-    renderWithProviders(<SettingsBar close={mockClose} />);
+  test("should be able to toggle settings", async () => {
+    const { store } = renderWithProviders(<SettingsBar close={mockClose} />);
 
+    // toggle for the first time
     fireEvent.click((await findInputByLabelMatcher(autoPlayMatcher)));
     expect((await findInputByLabelMatcher(autoPlayMatcher)).checked)
       .toBe(!initialSettingsState.autoplay);
 
+    expect(selectAutoplay(store.getState())).toBe(!initialSettingsState.autoplay);
+
+    // toggle for the second time
     fireEvent.click((await findInputByLabelMatcher(autoPlayMatcher)));
     expect((await findInputByLabelMatcher(autoPlayMatcher)).checked)
       .toBe(initialSettingsState.autoplay);
+
+     expect(selectAutoplay(store.getState())).toBe(initialSettingsState.autoplay)
   });
 
-  test("Can change playmode", async () => {
-    renderWithProviders(<SettingsBar close={mockClose} />);
+  test("should be able to change play mode", async () => {
+    const { store } = renderWithProviders(<SettingsBar close={mockClose} />);
 
     const newPlaymode = PlayMode.SEQUENCE;
 
     // check that truly selecting a new playmode
-    expect(newPlaymode).not.toBe(initialSettingsState.playMode);
+    expect(selectPlayMode(store.getState())).not.toBe(newPlaymode);
 
     // select a new option
     changeOptionByLabel(playmodeMatcher, newPlaymode);
 
     expect(await findInputByLabelMatcher(playmodeMatcher))
       .toHaveValue(newPlaymode);
-  });
 
+    expect(selectPlayMode(store.getState())).toBe(newPlaymode);
+  });
 });
